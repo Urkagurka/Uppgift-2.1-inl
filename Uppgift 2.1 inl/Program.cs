5f776367f2db4f4bf678cde7b53a43e89eee4a04
alve06001 <alve06001@E-PW0234LA>
2023-09-15 12:57:50 +0200

    Add project files.

---
+++ Uppgift 2.1 inl/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Vad är din favorit bok");
+            string favoritbooook = (Console.ReadLine());
+            Console.WriteLine(" din favoritbok är " + favoritbooook);    
+        }
+    }
+}
